f39854bfe5b84cfec762252b0cbec27973b2db94
Joomy Korkut <joomy@cattheory.com>
2019-03-06 00:20:15 -0500

    change from list to string

--- Verif_levenshtein.v
+++ Verif_levenshtein.v
@@ -1,50 +1,24 @@
 Require Import Coq.Strings.String.
 Require Import Coq.Strings.Ascii.
-Require Import Coq.Lists.List.
 Require Import Coq.Init.Peano.
 Require Import Omega.
 Require Import Coq.Program.Equality.
 
-Definition lstring := list ascii.
+Open Scope string_scope.
+Print string.
+Infix "::" := String (at level 60, right associativity) : string_scope.
 
-Section Isomorphism.
+Inductive edit : string -> string -> Type :=
+| insertion (a : ascii) {s : string} : edit s (a :: s)
+| deletion (a : ascii) {s : string} : edit (a :: s) s
+| update (a' : ascii) (a : ascii) {s : string} : edit (a' :: s) (a :: s).
 
-Fixpoint to_string (l : lstring) : string :=
-  match l with
-  | nil => EmptyString
-  | cons x xs => String x (to_string xs)
-  end.
-Fixpoint to_lstring (s : string) : lstring :=
-  match s with
-  | EmptyString => nil
-  | String x xs => cons x (to_lstring xs)
-  end.
-Lemma lstring_same : forall l, to_lstring (to_string l) = l.
-Proof. intro l; induction l; simpl; f_equal; auto. Defined.
+Inductive chain : string -> string -> Type :=
+| empty : chain "" ""
+| skip {a : ascii} {s t : string} : chain s t -> chain (a :: s) (a :: t)
+| change {s t u : string} : edit s t -> chain t u -> chain s u.
 
-Lemma string_same : forall s, to_string (to_lstring s) = s.
-Proof. intro s; induction s; simpl; f_equal; auto. Defined.
-
-Definition on_string (f : lstring -> lstring) (s : string) : string :=
-  to_string (f (to_lstring s)).
-Definition on_lstring (f : string -> string) (l : lstring) : lstring :=
-  to_lstring (f (to_string l)).
-End Isomorphism.
-
-Open Scope list_scope.
-Import ListNotations.
-
-Inductive edit : lstring -> lstring -> Type :=
-| insertion (a : ascii) {s : lstring} : edit s (a :: s)
-| deletion (a : ascii) {s : lstring} : edit (a :: s) s
-| update (a' : ascii) (a : ascii) {s : lstring} : edit (a' :: s) (a :: s).
-
-Inductive chain : lstring -> lstring -> Type :=
-| empty : chain [] []
-| skip {a : ascii} {s t : lstring} : chain s t -> chain (a :: s) (a :: t)
-| change {s t u : lstring} : edit s t -> chain t u -> chain s u.
-
-Fixpoint chain_changes {s t : lstring} (c : chain s t) : nat :=
+Fixpoint chain_changes {s t : string} (c : chain s t) : nat :=
   match c with
   | empty => 0
   | skip c' => chain_changes c'
@@ -54,14 +28,6 @@ Fixpoint chain_changes {s t : lstring} (c : chain s t) : nat :=
 Lemma same_chain : forall s, chain s s.
 intros s. induction s; constructor. auto.
 Defined.
-
-Lemma ch1 : chain (to_lstring "h") (to_lstring "ah").
-simpl.
-apply (@change _ ["a"%char;"h"%char]).
-constructor.
-apply same_chain.
-Defined.
-Eval compute in ch1.
 
 Lemma insert_chain : forall c s1 s2, chain s1 s2 -> chain s1 (c :: s2).
 intros c s1 s2 C.
@@ -75,13 +41,16 @@ induction s2; constructor; auto.
 apply insert_chain; auto.
 Defined.
 
-(* transparent version of app_nil_r *)
-Lemma tr_app_nil_r : forall {A : Type} (l : list A), l ++ [] = l.
+(* transparent string version of app_nil_r *)
+Lemma tr_app_empty_r : forall {A : Type} (l : string), l ++ "" = l.
 intros A l; induction l. auto. simpl; rewrite IHl; auto.
 Defined.
 
-Lemma inserts_chain_nil : forall s, chain [] s.
-intros s; pose (inserts_chain s nil); rewrite (tr_app_nil_r s) in *; auto.
+Lemma inserts_chain_empty : forall s, chain "" s.
+intros s.
+induction s; simpl.
+constructor.
+apply insert_chain. auto.
 Defined.
 
 Lemma delete_chain : forall c s1 s2, chain s1 s2 -> chain (c :: s1) s2.
@@ -97,8 +66,10 @@ apply delete_chain.
 auto.
 Defined.
 
-Lemma deletes_chain_nil : forall s, chain s [].
-intros s; pose (deletes_chain s nil); rewrite (tr_app_nil_r s) in *; auto.
+Lemma deletes_chain_empty : forall s, chain s "".
+intros s.
+induction s; simpl.
+constructor. apply delete_chain. auto.
 Defined.
 
 Lemma update_chain : forall c c' s1 s2, chain s1 s2 -> chain (c :: s1) (c' :: s2).
@@ -118,7 +89,7 @@ intros s t x xs y ys eq1 eq2 ceq C.
 subst. apply skip. auto.
 Defined.
 
-Lemma aux_both_empty : forall s t, s = [] -> t = [] -> chain s t.
+Lemma aux_both_empty : forall s t, s = "" -> t = "" -> chain s t.
 intros s t eq1 eq2. subst. constructor.
 Defined.
 
@@ -129,34 +100,36 @@ Definition min_app {t : Type} (x y z : t) (f : t -> nat) : t :=
   | false => match (Nat.leb n2 n3) with | true => y | false => z end
   end.
 
-Fixpoint levenshtein_chain (s : lstring) :=
-  fix levenshtein_chain1 (t : lstring) : chain s t :=
+Fixpoint levenshtein_chain (s : string) :=
+  fix levenshtein_chain1 (t : string) : chain s t :=
     (match s as s', t as t' return s = s' -> t = t' -> chain s t with
-    | nil , nil => (* redundant but whatever *)
+    | "" , "" => (* redundant but whatever *)
         fun eq1 eq2 => aux_both_empty s t eq1 eq2
-    | nil , _ =>
+    | "" , _ =>
         fun eq1 eq2 =>
-          ltac:(rewrite eq1; apply (inserts_chain_nil t))
-    | cons y ys , nil =>
+          ltac:(rewrite eq1; apply (inserts_chain_empty t))
+    | y :: ys , "" =>
         fun eq1 eq2 =>
-          ltac:(rewrite eq1, eq2; apply (deletes_chain_nil (y :: ys)))
-    | cons x xs, cons y ys =>
+          ltac:(rewrite eq1, eq2; apply (deletes_chain_empty (y :: ys)))
+    | x :: xs, y :: ys =>
       fun eq1 eq2 =>
         match ascii_dec x y with
         | left ceq => aux_eq_char s t x xs y ys eq1 eq2 ceq (levenshtein_chain xs ys)
         | right neq =>
-            let r1 := levenshtein_chain1 ys in
-            let r2 := levenshtein_chain xs (y :: ys) in
-            let r3 := levenshtein_chain xs ys in
-            let r1' : chain s t :=
-                aux_insert s t x xs y ys eq1 eq2 r1 in
-            let r2' : chain s t :=
-                ltac:(rewrite eq1, eq2; apply (delete_chain x xs (y :: ys) r2)) in
-            let r3' : chain s t :=
-                ltac:(rewrite eq1, eq2; apply (update_chain x y xs ys r3)) in
-            min_app r1' r2' r3' chain_changes
+          let r1 := levenshtein_chain1 ys in
+          let r2 := levenshtein_chain xs (y :: ys) in
+          let r3 := levenshtein_chain xs ys in
+          let r1' : chain s t :=
+              aux_insert s t x xs y ys eq1 eq2 r1 in
+          let r2' : chain s t :=
+              ltac:(rewrite eq1, eq2;
+                    apply (delete_chain x xs (y :: ys) r2)) in
+          let r3' : chain s t :=
+              ltac:(rewrite eq1, eq2;
+                    apply (update_chain x y xs ys r3)) in
+          min_app r1' r2' r3' chain_changes
         end
     end) (eq_refl s) (eq_refl t).
 
-Eval compute in (levenshtein_chain (to_lstring "x") (to_lstring "X")).
-Eval compute in (chain_changes (levenshtein_chain (to_lstring "joomy") (to_lstring "Joomy"))).
+Eval compute in (levenshtein_chain "x" "X").
+Eval compute in (chain_changes (levenshtein_chain "joomy" "Joomy")).
